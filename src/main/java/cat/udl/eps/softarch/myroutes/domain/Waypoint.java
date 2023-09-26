6c62e11755407555c4b8317a37836a98bc6790ee
ZcR-01 <zihan1048@live.com>
2023-09-26 16:16:48 +0200

    Create waypoint entity

---
+++ src/main/java/cat/udl/eps/softarch/myroutes/domain/Waypoint.java
@@ -0,0 +1,32 @@
+package cat.udl.eps.softarch.myroutes.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import lombok.Data;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+public class Waypoint extends UriEntity<Long> {
+
+    @Id
+    @GeneratedValue()
+    private long id;
+
+    @NotNull
+    @NotBlank
+    private String title;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @NotBlank
+    private String type;
+}
